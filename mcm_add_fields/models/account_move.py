c4929a09070cd7ee28ebbcdb7701d6355787dba5
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-02-02 14:33:05 +0100

    [UPD] invoice paid amount condition

--- mcm_add_fields/models/account_move.py
+++ mcm_add_fields/models/account_move.py
@@ -26,7 +26,7 @@ class AccountMove(models.Model):
             paid_amount = 0.0
 
             for payment in payments:
-                if payment.company_id==rec.company_id:
+                if payment.company_id==rec.company_id: 
                     paid_amount += payment.amount
             rec.mcm_paid_amount = paid_amount
 
