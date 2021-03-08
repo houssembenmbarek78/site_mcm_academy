599348a746f6524c290301029ef005c7ad1ee828
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-03-08 15:42:59 +0100

    [UPD] init sign template

--- hide_auto_sign_portal/static/src/js/portal.js
+++ hide_auto_sign_portal/static/src/js/portal.js
@@ -4,15 +4,24 @@ odoo.define('hide_auto_sign_portal.portal', function (require) {
 var publicWidget = require('web.public.widget');
 
 publicWidget.registry.SignTemplate = publicWidget.Widget.extend({
-     selector: '.o_web_sign_name_and_signature',
+     selector: '.o_portal_sidebar',
 //     events: {
 //        'click a[id="accept_and_sign"]': 'hide_auto_sign',
 //        'mouseover a[id="accept_and_sign"]': 'hide_auto_sign',
 //        'click a[id="footer_accept_and_sign"]': 'hide_auto_sign',
 //        'mouseover a[id="footer_accept_and_sign"]': 'hide_auto_sign',
 //    },
-    start: function() {
-        var self = this;
+//    start: function() {
+//         var self = this;
+//         console.log('aazzaazzazazaazazazaaza');
+//         this.$('a.o_web_sign_auto_button').css('display','none');
+//         this.$('a.o_web_sign_load_button').css('display','none');
+//         this.$('a.o_web_sign_draw_button').html('Signature');
+//         this.$autoButton=this.$('a.o_web_sign_auto_button');
+//         console.log(this.$autoButton.inner)
+//    },
+    init: function () {
+         var self = this;
          console.log('aazzaazzazazaazazazaaza');
          this.$('a.o_web_sign_auto_button').css('display','none');
          this.$('a.o_web_sign_load_button').css('display','none');
