cae4191ab567290accc9ea8b5b872cec44da8ea8
Ines Lahmar <ilahmar@digimoov.fr>
2021-03-18 16:05:34 +0100

    [ADD]inherit methode of res.users_model in users_model to get user's password

---
+++ test/models/users.py
@@ -0,0 +1,17 @@
+from odoo import models,fields,api
+
+class Users(models.Model):
+  _inherit = "res.users"
+
+  password360= fields.Char(string="password360")
+  #Héritage de methode _set_password pour recuperer le mot de passe apres l'inscription et avant le cryptage
+  #Idée Ahlem
+  def _set_password(self):
+     for user in self:
+       user.password360=user.password
+     return super(Users, self)._set_password()
+
+
+
+
+
--- test/models/userstat.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,fields,api
-
-class userstat(models.Model):
-  _name = "test.userstat"
-  _description = "statistiques"
-
-
-
-
