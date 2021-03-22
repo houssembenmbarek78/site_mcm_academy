5c41cd11334e7c3a65bbb01dcf5f24ea31024c29
Ahlem Zoccar <azaccar@digimoov.fr>
2021-03-22 17:04:31 +0100

    [UPD] Pytesseract in ocrdocument class to extract text from image

--- ocrdocument/models/models.py
+++ ocrdocument/models/models.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from cv2 import cv2
+from pytesseract import pytesseract
 
 
 
@@ -15,7 +16,8 @@ class ocrdocument(models.Model):
 # Extract text from image with open cv into text
     def trycv(self):
         for r in self:
-           img2 = cv2.imread('image.png')
-           cv2.imshow('Result2', img2)
-           cv2.waitKey(0)
-
+           # img2 = cv2.imread('image.png')
+           # cv2.imshow('Result2', img2)
+           # cv2.waitKey(0)
+           image = pytesseract.image_to_string('image.png')
+           print(image)
