208eb0fb92d0fe1c781b23b3c904d664d0723dbd
Ines Lahmar <ilahmar@digimoov.fr>
2021-03-23 14:48:37 +0100

    [UPD] update field in model groupe

--- test/models/partner.py
+++ test/models/partner.py
@@ -14,8 +14,8 @@ class partner(models.Model):
     session_ids = fields.Many2many('test.session', string='attended_sessions',
                                    readonly=True)
 
-    groupe_admin_ids= fields.Many2many('test.groupe', string='groupes à gérer')
-    groupe_user_ids =fields.Many2many('test.groupe', string='groupes à suivre')
+    # groupe_admin_ids= fields.Many2many('test.groupe', string='groupes à gérer')
+    # groupe_user_ids =fields.Many2many('test.groupe', string='groupes à suivre')
 
     #Champs pour recuperer les statistiques
     last_login=fields.Char(string="derniere Connexion")
