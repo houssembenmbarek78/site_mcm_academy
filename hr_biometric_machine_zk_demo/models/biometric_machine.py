9eba4887d92f57165be06fd84974c2d4c1532ba9
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-04-01 12:31:34 +0100

    [UPD] test try connection

--- hr_biometric_machine_zk_demo/models/biometric_machine.py
+++ hr_biometric_machine_zk_demo/models/biometric_machine.py
@@ -30,6 +30,7 @@ class zkMachine(models.Model):
             conn = ''
             try:
                 conn = zk.connect()
+                raise ValidationError(_('teeest!'))
                 users = conn.get_users()
             except Exception as e:
                 raise UserError('The connection has not been achieved')
