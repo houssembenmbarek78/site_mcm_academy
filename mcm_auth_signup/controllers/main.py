d1f4384d6437d39e5ada959e2ac36ba16c99cab8
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-04-09 17:46:45 +0100

    [UPD] Rendre les majuscules en minuscules dans l'email lors de création de compte sur le site web

--- mcm_auth_signup/controllers/main.py
+++ mcm_auth_signup/controllers/main.py
@@ -2,11 +2,14 @@
 
 import logging
 import werkzeug
+import odoo
 from odoo import http, _
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 from odoo.addons.auth_signup.models.res_users import SignupError
 from odoo.exceptions import UserError
 from odoo.http import request
+from odoo.addons.web.controllers.main import Home
+from odoo.addons.web.controllers import main
 
 _logger = logging.getLogger(__name__)
 
@@ -48,10 +51,13 @@ class AuthSignupHome(AuthSignupHome):
 
         if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
+                qcontext['login'] = qcontext['login'].replace(' ', '').lower()
                 self.do_signup(qcontext)
                 # Send an account creation confirmation email
                 if qcontext.get('token'):
-                    user_sudo = request.env['res.users'].sudo().search([('login', "=", qcontext.get('login').replace(' ', '').lower())])
+
+                    user_sudo = request.env['res.users'].sudo().search(
+                        [('login', "=", qcontext.get('login').replace(' ', '').lower())])
                     template = request.env.ref('auth_signup.mail_template_user_signup_account_created',
                                                raise_if_not_found=False)
                     if user_sudo and template:
@@ -59,15 +65,19 @@ class AuthSignupHome(AuthSignupHome):
                             lang=user_sudo.lang,
                             auth_login=werkzeug.url_encode({'auth_login': user_sudo.email}),
                         ).send_mail(user_sudo.id, force_send=True)
-                    user_sudo = request.env['res.users'].sudo().search([('login', "=", qcontext.get('login').replace(' ', '').lower())])
+                    user_sudo = request.env['res.users'].sudo().search(
+                        [('login', "=", qcontext.get('login').replace(' ', '').lower())])
                     if user_sudo:
                         user_sudo.street = str(request.website.name)
-                    kw['login'] = qcontext.get('login').replace(' ', '').lower()
+                kw['login'] = qcontext.get('login').replace(' ', '').lower()
+                print('kw: ', kw)
+                print('args: ', args)
                 return self.web_login(*args, **kw)
             except UserError as e:
                 qcontext['error'] = e.name or e.value
             except (SignupError, AssertionError) as e:
-                if request.env["res.users"].sudo().search([("login", "=", qcontext.get("login").replace(' ', '').lower())]):
+                if request.env["res.users"].sudo().search(
+                        [("login", "=", qcontext.get("login").replace(' ', '').lower())]):
                     qcontext["error"] = _("Another user is already registered using this email address.")
                 else:
                     _logger.error("%s", e)
@@ -75,4 +85,13 @@ class AuthSignupHome(AuthSignupHome):
 
         response = request.render('auth_signup.signup', qcontext)
         response.headers['X-Frame-Options'] = 'DENY'
+        return response
+
+
+class Home(Home):
+
+    @http.route('/web/login', type='http', auth="public")
+    def web_login(self, redirect=None, **kw):
+        request.params['login'] = request.params['login'].replace(' ','').lower()
+        response=super(Home, self).web_login(redirect=redirect,**kw)
         return response
