399fac812c61ce652c3a6ebfc8b9f670f495a3fb
digimoovtakwa <tmejri@digimoov.fr>
2021-04-26 12:21:05 +0200

    [ADD] Add field boolean with name this_is_technical_field to base the action server on it.

--- partner_exam/models/partner.py
+++ partner_exam/models/partner.py
@@ -7,3 +7,23 @@ class resComapny(models.Model):
     _inherit = "res.partner"
 
     note_exam=fields.Char("Note d'examen blanc")
+    note_exam_id = fields.One2many('info.examen', 'partner_id')
+    note_exam_count = fields.Integer(compute="compute_notes_exams_count")
+    this_is_technical_field = fields.Boolean(readonly=True, default=True)
+
+    def compute_notes_exams_count(self):
+        for record in self:
+            record.note_exam_count = self.env['info.examen'].search_count(
+                [('partner_id', 'child_of', self.id)])
+
+    def get_notes_history(self):
+        self.ensure_one()
+        return {
+            "name": self.name,
+            "view_type": "form",
+            "view_mode": "tree,form",
+            "res_model": "info.examen",
+            "type": "ir.actions.act_window",
+            "domain": [("partner_id", "child_of", self.id)],
+            'context': {'default_note_exam_id': self.note_exam_id.ids},
+        }
