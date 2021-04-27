92e024479bf06aa08bc74ce7e501846bc980176e
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-04-27 08:35:45 +0100

    [UPD] incoming rejected mails

--- helpdesk_create_ticket/models/helpdesk_ticket.py
+++ helpdesk_create_ticket/models/helpdesk_ticket.py
@@ -87,7 +87,7 @@ class HelpdeskTicket(models.Model):
         ]
         for rec in list_value:
             if any(email in rec['partner_email'] for email in rejected_mails):
-                sys.exit()
+                _logger.error("%s is a rejected mail",rec['partner_email'])
         tickets = super(HelpdeskTicket, self).create(list_value)
         for rec in list_value:
             if 'partner_id' in rec:
