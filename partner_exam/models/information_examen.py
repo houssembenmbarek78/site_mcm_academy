deb81651e5d3fb25fe9804f027272f11d9a468b0
digimoovtakwa <tmejri@digimoov.fr>
2021-05-26 15:54:18 +0200

    [DEL] Delete Test default,

--- partner_exam/models/information_examen.py
+++ partner_exam/models/information_examen.py
@@ -14,34 +14,7 @@ class NoteExamen(models.Model):
     partner_id = fields.Many2one('res.partner', string="Client")
     epreuve_a = fields.Float(string="Epreuve A(QCM):")
     epreuve_b = fields.Float(string="Epreuve B(QRO)")
-
-    @api.onchange('epreuve_a', 'epreuve_b', 'presence')
-    def _compute_moyenne_generale(self):
-        """ This function used to auto display some result
-        like the "Moyenne Generale" & "Mention" & "Resultat" """
-        for rec in self:
-            rec.moyenne_generale = (rec.epreuve_a + rec.epreuve_b) / 2
-            if rec.epreuve_a >= 10 and rec.epreuve_b >= 8 and rec.moyenne_generale >= 12:
-                rec.moyenne_generale = rec.moyenne_generale
-                rec.mention = 'recu'
-                rec.resultat = 'recu'
-                rec.presence = 'present'
-            else:
-                # reset your fields
-                rec.epreuve_a = rec.epreuve_a
-                rec.epreuve_b = rec.epreuve_b
-                rec.mention = 'ajourne'
-                rec.resultat = 'ajourne'
-                if rec.epreuve_a > 1 and rec.epreuve_a < 21:
-                    rec.presence = 'present'
-                elif rec.epreuve_a < 1 and rec.epreuve_b < 1:
-                    rec.presence = 'Absent'
-                if rec.epreuve_b > 1 and rec.epreuve_b < 21:
-                    rec.presence = 'present'
-                elif rec.epreuve_a < 1 and rec.epreuve_b < 1:
-                    rec.presence = 'Absent'
-                    
-    moyenne_generale = fields.Float(string="Moyenne Générale", track_visibility='always', default="_compute_moyenne_generale")
+    moyenne_generale = fields.Float(string="Moyenne Générale", track_visibility='always')
     mention = fields.Selection(selection=[
         ('recu', 'reçu'),
         ('ajourne', 'ajourné')],
@@ -68,6 +41,31 @@ class NoteExamen(models.Model):
         ('Absent', 'Absent')],
         string="Présence")
 
+    @api.onchange('epreuve_a', 'epreuve_b', 'presence')
+    def _compute_moyenne_generale(self):
+        """ This function used to auto display some result
+        like the "Moyenne Generale" & "Mention" & "Resultat" """
+        for rec in self:
+            rec.moyenne_generale = (rec.epreuve_a + rec.epreuve_b) / 2
+            if rec.epreuve_a >= 10 and rec.epreuve_b >= 8 and rec.moyenne_generale >= 12:
+                rec.moyenne_generale = rec.moyenne_generale
+                rec.mention = 'recu'
+                rec.resultat = 'recu'
+                rec.presence = 'present'
+            else:
+                # reset your fields
+                rec.epreuve_a = rec.epreuve_a
+                rec.epreuve_b = rec.epreuve_b
+                rec.mention = 'ajourne'
+                rec.resultat = 'ajourne'
+                if rec.epreuve_a > 1 and rec.epreuve_a < 21:
+                    rec.presence = 'present'
+                elif rec.epreuve_a < 1 and rec.epreuve_b < 1:
+                    rec.presence = 'Absent'
+                if rec.epreuve_b > 1 and rec.epreuve_b < 21:
+                    rec.presence = 'present'
+                elif rec.epreuve_a < 1 and rec.epreuve_b < 1:
+                    rec.presence = 'Absent'
 
 
 
