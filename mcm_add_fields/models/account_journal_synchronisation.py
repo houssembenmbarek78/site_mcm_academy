bb76d13645e8e6b7dc0040a6ac13e0c944ae4497
seifeddinne <smabrouk@digimoov.fr>
2021-06-09 09:03:30 +0200

    [UPD] Fix Process Facturation par methodes_payment cpf/carte_bleu Synchronisation facture _ancienne

---
+++ mcm_add_fields/models/account_journal_synchronisation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+#Ce programme a été modifié par seifeddinne le 22/03/2021
+#Modification du process de la facturation
+#Modification de l'aperçu de la facturation
+from odoo import api, fields, models,_
+from odoo.exceptions import RedirectWarning, UserError, ValidationError
+from datetime import datetime
+
+
+class AccountJournalSynchronisation(models.Model):
+    _inherit = "account.journal"
+
+    # Synchroniser les factures recentes
+    def synchronisation_recent_invoice(self):
+        factures = self.env['account.move'].search([])
+        date_du_jour = fields.Date(string='Date-Aujourdui', default=datetime.today())
+        for facture in factures:
+            if ((facture.cpf_solde_invoice == True and facture.invoice_date <= date_du_jour ) or (facture.cpf_acompte_invoice == True and  facture.invoice_date < date_du_jour) or (facture.invoice_user_id == 'ZOÉ' and facture.invoice_date < date_du_jour)):
+                facture.methodes_payment = 'cpf'
+            elif (facture.invoice_user_id != 'ZOÉ'):
+                facture.methodes_payment = 'cartebleu'
+
