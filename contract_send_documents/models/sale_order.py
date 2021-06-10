b01af99614ed2cdf33827536af0568f6eb3b57ef
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-06-10 16:57:50 +0100

    [UPD]  fix send mail of documents

--- contract_send_documents/models/sale_order.py
+++ contract_send_documents/models/sale_order.py
@@ -60,18 +60,18 @@ class SaleOrder(models.Model):
                     order.with_context(force_send=True).message_post_with_template(template_id,
                                                                                    composition_mode='comment',
                                                                                    email_layout_xmlid="contract_send_documents.portal_contract_document_mail")
-            subtype_id = self.env['ir.model.data'].xmlid_to_res_id('mt_note')
+            # subtype_id = self.env['ir.model.data'].xmlid_to_res_id('mt_note')
             attachments = self.env['ir.attachment'].search(
                 [("name", "=", "cerfa_11414-05.pdf"), ("description", "=", "cerfa_mail_template_attachment"),
                  ('res_model', "=", "sale.order")])
             for attachment in attachments:
                 attachment.sudo().unlink()
-            message = self.env['mail.message'].sudo().create({
-                'subject': 'Contrat signé',
-                'model': 'res.partner',
-                'res_id': self.partner_id.id,
-                'message_type': 'notification',
-                'subtype_id': subtype_id,
-                'body': 'Contrat signé par ' + str(values['signed_by']),
-            })
+            # message = self.env['mail.message'].sudo().create({
+            #     'subject': 'Contrat signé',
+            #     'model': 'res.partner',
+            #     'res_id': self.partner_id.id,
+            #     'message_type': 'notification',
+            #     'subtype_id': subtype_id,
+            #     'body': 'Contrat signé par ' + str(values['signed_by']),
+            # })
         return order
