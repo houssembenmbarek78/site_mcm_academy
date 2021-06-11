ce3d7d688d04e3e28141848ef669370112324ccc
digimoovtakwa <tmejri@digimoov.fr>
2021-06-11 10:56:42 +0200

    [UPD] Update session_ville.

--- mcm_session/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import session, module, action_type, module_details, stage, domain_formation, res_partner, sale_order, \
-    paymentTransaction, session_programme, product_template , account,MessageWizard
