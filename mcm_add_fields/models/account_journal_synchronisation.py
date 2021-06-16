1988db87129a1a1b5bc48624ffce58dcd1a652e8
seifeddinne <smabrouk@digimoov.fr>
2021-06-16 14:29:10 +0200

    [ADD] Fix Process Facturation par methodes_payment Methodes_payment required date process_synchronisation

--- mcm_add_fields/models/account_journal_synchronisation.py
+++ mcm_add_fields/models/account_journal_synchronisation.py
@@ -5,8 +5,7 @@
 #Modification de l'aperçu de la facturation
 from odoo import api, fields, models,_
 from odoo.exceptions import RedirectWarning, UserError, ValidationError
-from datetime import datetime
-import datetime
+from datetime import datetime , date
 
 class AccountJournalSynchronisation(models.Model):
     _inherit = "account.journal"
@@ -14,13 +13,19 @@ class AccountJournalSynchronisation(models.Model):
     # Synchroniser les factures recentes
     def synchronisation_recent_invoice(self):
       factures = self.env['account.move'].search([])
-      # date_du_jour = fields.Date(string='Date-Aujourdui', default=datetime.today())
-
-      My_date =datetime.datetime.strptime('08062021', '%d%m%Y').date()
+      My_date = date(2021, 4, 28)
+      daysDiff = 0
 
       for facture in factures:
-        if (facture.cpf_solde_invoice == True  or facture.cpf_acompte_invoice == True or facture.invoice_user_id == 'ZOÉ') :
-            facture.methodes_payment = 'cpf'
-            facture.pourcentage_acompte = 0
-        elif facture.invoice_user_id != 'ZOÉ':
-            facture.methodes_payment = 'cartebleu'
+        daysDiff = ((My_date) - facture.invoice_date).days
+        if (facture.invoice_date and My_date) :
+          if daysDiff > 0 :
+             if (facture.cpf_solde_invoice == True  or facture.cpf_acompte_invoice == True or facture.invoice_user_id == 'ZOÉ' ) :
+                facture.methodes_payment = 'cpf'
+                facture.pourcentage_acompte = 0
+             elif facture.invoice_user_id != 'ZOÉ' :
+                facture.methodes_payment = 'cartebleu'
+
+
+
+
