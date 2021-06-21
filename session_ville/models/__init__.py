f56d6fa0eda724da7a2140873c6e4400d439688c
digimoovtakwa <tmejri@digimoov.fr>
2021-06-21 15:25:31 +0200

    [UPD] Addliste des ville "session_ville_id" dans l'interface client et module.

--- session_ville/models/__init__.py
+++ session_ville/models/__init__.py
@@ -2,6 +2,5 @@ from . import session_ville
 from . import adresse_centre_examen
 from . import inherit_mcmacademy_session
 from . import inherit_info_examen
-from . import inherit_res_partner
 from . import inherit_mcmacademy_module
 
