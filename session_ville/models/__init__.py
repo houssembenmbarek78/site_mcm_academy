a88c584769c4c6156ce583b645b4f1f1481a71a4
digimoovtakwa <tmejri@digimoov.fr>
2021-06-22 16:33:04 +0200

    [ADD] Ajouter Session ville module.

--- session_ville/models/__init__.py
+++ session_ville/models/__init__.py
@@ -2,3 +2,6 @@ from . import session_ville
 from . import adresse_centre_examen
 from . import inherit_mcmacademy_session
 from . import inherit_info_examen
+from . import inherit_res_partner
+from . import inherit_mcmacademy_module
+
