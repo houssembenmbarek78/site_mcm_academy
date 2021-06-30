49d96b66297a40b9a8ca8de0f228886bd9f4f8c1
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-06-30 17:05:21 +0100

    [UPD] fix MCM ACADEMY view upload documents & change url from '/new_documents' to '/charger_mes_documents'

--- mcm_contact_documents/controllers/main.py
+++ mcm_contact_documents/controllers/main.py
@@ -1079,9 +1079,14 @@ class CustomerPortal(CustomerPortal):
         name = http.request.env.user.name
         email = http.request.env.user.email
         partner_id = http.request.env.user.partner_id
-        return http.request.render('mcm_contact_documents.mcm_contact_document_charger_mes_documents', {
-            'email': email, 'name': name, 'partner_id': partner_id, 'error_identity': '', 'error_permis': '',
-            'error_identity_number': '', 'error_permis_number': '', 'error_domicile': ''})
+        if request.website.id==2: # id 2 of website in database means website DIGIMOOV
+            return http.request.render('mcm_contact_documents.mcm_contact_document_charger_mes_documents', {
+                'email': email, 'name': name, 'partner_id': partner_id, 'error_identity': '', 'error_permis': '',
+                'error_identity_number': '', 'error_permis_number': '', 'error_domicile': ''})
+        elif request.website.id==1: # id 1 of website in database means website MCM ACADEMY
+            return http.request.render('mcm_contact_documents.mcm_contact_documents_charger_mes_documents_mcm', {
+                'email': email, 'name': name, 'partner_id': partner_id, 'error_identity': '', 'error_permis': '',
+                'error_identity_number': '', 'error_permis_number': '', 'error_domicile': ''})
 
     def _document_get_page_view_values(self, document, access_token, **kwargs):
         values = {
