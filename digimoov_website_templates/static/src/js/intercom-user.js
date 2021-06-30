876493d522fef60c8d1a634e724c4c28fc18ac86
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-06-30 12:51:33 +0100

    [UPD] fix intercom logged in user

--- digimoov_website_templates/static/src/js/intercom-user.js
+++ digimoov_website_templates/static/src/js/intercom-user.js
@@ -1,6 +1,8 @@
+var user_name=document.getElementById("intercom_user_name_connected").value;
+var user_email=document.getElementById("intercom_user_email_connected").value;
 window.intercomSettings = {
     app_id: "pe98h7lm",
-    name: "<%= current_user.name %>", // Full name
-    email: "<%= current_user.email %>", // Email address
+    name: user_name, // Full name
+    email: user_email // Email address
     created_at: "<%= current_user.created_at.to_i %>" // Signup date as a Unix timestamp
 };
