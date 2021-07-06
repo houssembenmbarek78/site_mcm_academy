7274397cb960d124832b5569dd1643460cfb9277
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-07-06 15:46:30 +0100

    [UPD] add intercom script in logged in user

--- digimoov_website_templates/static/src/js/intercom-user-config.js
+++ digimoov_website_templates/static/src/js/intercom-user-config.js
@@ -18,7 +18,7 @@
       var s = d.createElement("script");
       s.type = "text/javascript";
       s.async = true;
-      s.src = "https://widget.intercom.io/widget/waw4riyk";
+      s.src = "https://widget.intercom.io/widget/pe98h7lm";
       var x = d.getElementsByTagName("script")[0];
       x.parentNode.insertBefore(s, x);
     };
