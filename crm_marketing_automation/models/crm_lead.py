2edd4dd99e03b6f10acf649f5ebfa5a92e1ee90e
Ines Lahmar <ilahmar@digimoov.fr>
2021-07-07 17:08:25 +0200

    [UPD]related field

--- crm_marketing_automation/models/crm_lead.py
+++ crm_marketing_automation/models/crm_lead.py
@@ -9,7 +9,7 @@ from datetime import date,datetime
 class CRM(models.Model):
     _inherit = "crm.lead"
 
-    num_dossier=fields.Char(string="numéro de dossier")
-    num_tel=fields.Char(string="numéro de téléphone")
-    email=fields.Char(string="email")
-
+    num_dossier=fields.Char(string="numéro de dossier",related='partner_id.numero_cpf')
+    num_tel=fields.Char(string="numéro de téléphone",related='partner_id.phone')
+    email=fields.Char(string="email",related='partner_id.email')
+    mode_financement=fields.Char(string="mode_financement",related='partner_id.mode_de_financement')
