beb77b0fc993483b620ef7646ef6863416306df3
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-07-08 12:26:59 +0100

    [UPD] fix merge error add fileds to company

--- partner_exam/__manifest__.py
+++ partner_exam/__manifest__.py
@@ -26,7 +26,7 @@
         'security/ir.model.access.csv',
         'views/partner.xml',
         'views/notes_examens_partner.xml',
-        'views/add_signature.xml',
+        # 'views/add_signature.xml',
         'report/menu.xml',
         'report/generate_releve_de_notes.xml',
         'report/convocation_contact.xml',
